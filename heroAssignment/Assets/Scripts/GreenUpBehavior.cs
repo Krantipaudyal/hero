04d499c8f4af93a2e292e70738fb45667a5556f3
Krantipaudyal <krantipaudyal@gmail.com>
2021-04-14 19:02:52 -0700

    added web gl

--- heroAssignment/Assets/Scripts/GreenUpBehavior.cs
+++ heroAssignment/Assets/Scripts/GreenUpBehavior.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class GreenUpBehavior : MonoBehaviour
 {
     public float speed = 10f;
+    public float mHeroRotateSpeed = 90f/2f; //90 degrees in 2 sec
+
+    public bool mFollowMousePosition  = false; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,26 +18,40 @@ public class GreenUpBehavior : MonoBehaviour
     // Update is called once per frame
  void Update()
     {
-        Vector3 pos = transform.position;
+         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.M))
         {
-            pos.y += speed * Time.deltaTime;
+            mFollowMousePosition = !mFollowMousePosition;
+
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if(mFollowMousePosition)
         {
-            pos.y -= speed * Time.deltaTime;
+            pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0f; //
         }
+        else{
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            pos.x += speed * Time.deltaTime;
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                pos += ((speed * Time.smoothDeltaTime) * transform.up);
+            }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            pos.x -= speed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.S))
+            {
+                pos -= ((speed * Time.smoothDeltaTime) * transform.up);        }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(transform.forward, -mHeroRotateSpeed * Time.smoothDeltaTime);
+            
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(transform.forward, mHeroRotateSpeed * Time.smoothDeltaTime);
+            }
         }
 
         transform.position = pos;
