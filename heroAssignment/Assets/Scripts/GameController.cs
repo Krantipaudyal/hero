fcf85fa6a0b190b0d3d5fcf8c7612072239c3044
Krantipaudyal <krantipaudyal@gmail.com>
2021-04-15 00:23:14 -0700

    fixed everything

--- heroAssignment/Assets/Scripts/GameController.cs
+++ heroAssignment/Assets/Scripts/GameController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    private int maxPlanes = 10;
+    private int numberofPlanes = 0;
+   // public gameObject Plane;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +28,24 @@ public class GameController : MonoBehaviour
             Application.Quit();
     #endif
         }
+
+        if(numberofPlanes < maxPlanes)
+        {
+            //Bounds myBound = GetComponent <Renderer>().bounds;
+            CameraSupport s = Camera.main.GetComponent<CameraSupport>();
+            GameObject e = Instantiate(Resources.Load("Prefabs/Enemy") as GameObject);
+            Vector3 pos;
+            pos.x = s.GetWorldBounds().min.x + Random.value * s.GetWorldBounds().size.x;
+            pos.y = s.GetWorldBounds().min.y + Random.value * s.GetWorldBounds().size.y;
+            pos.z = 0;
+            e.transform.localPosition = pos;
+            ++numberofPlanes;
+
+        }
         
+    }
+    public void EnemyDestroyed()
+    {
+        --numberofPlanes; //decreases the current number of planes when destroyed
     }
 }
--- heroAssignment/Assets/Scripts/GreenUpBehavior.cs
+++ heroAssignment/Assets/Scripts/GreenUpBehavior.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GreenUpBehavior : MonoBehaviour
 {
-    public float speed = 10f;
+
+    public Text mEnemyCountTexxt = null;
+    private int mPlanesTouched = 0;
+
+    public float speed = 20f;
     public float mHeroRotateSpeed = 90f/2f; //90 degrees in 2 sec
 
+    public float cooldown = 0.2f; // .2 seconds of cooldown
+    public float fireNext = 0f;
     public bool mFollowMousePosition  = false; 
+
+    private GameController mGameController = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mGameController = FindObjectOfType<GameController>();
     }
 
     // Update is called once per frame
@@ -45,24 +54,130 @@ public class GreenUpBehavior : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 transform.Rotate(transform.forward, -mHeroRotateSpeed * Time.smoothDeltaTime);
+                //transform.Rotate(0,0,-mHeroRotateSpeed*Time.smoothDeltaTime);
+
             
             }
 
             if (Input.GetKey(KeyCode.A))
             {
                 transform.Rotate(transform.forward, mHeroRotateSpeed * Time.smoothDeltaTime);
+                //transform.Rotate(0,0,mHeroRotateSpeed*Time.smoothDeltaTime);
+
             }
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        triggerEggs();
+        transform.position = pos;
+    }
+
+    private void triggerEggs()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > fireNext )
         {
             // Prefab MUST BE locaed in Resources/Prefab folder!
             GameObject e = Instantiate(Resources.Load("Prefabs/Egg") as GameObject); 
             e.transform.localPosition = transform.localPosition;
             e.transform.rotation = transform.rotation;
-            Debug.Log("Spawn Eggs:" + e.transform.localPosition);
+            fireNext = Time.time + cooldown;
         }
+    }
 
-        transform.position = pos;
+    private void onTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Hero X Plane: onTriggerEnter2D");
+        if(collision.gameObject.tag == "Enemy")
+        {
+            mPlanesTouched += 1;
+           // mEnemyCountTexxt = " Planes touched = " + mPlanesTouched;
+            Destroy(collision.gameObject);
+            mGameController.EnemyDestroyed();
+        }
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class GreenUpBehaviour : MonoBehaviour
+// {
+//     public Text mEnemyCountText = null;
+//     public float speed = 10f;
+//     public float mHeroRotateSpeed = 100f / 2f; // 90-degrees in 2 seconds
+//     public bool mFollowMousePosition = true;
+//     // Start is called before the first frame update
+
+//     private int mPlanesTouched = 0;
+
+//     private GameController mGameGameController = null;
+
+//     void Start()
+//     {
+//         mGameGameController = FindObjectOfType<GameController>();
+//     }
+
+//     // Update is called once per frame
+
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.M))
+//         {
+//             mFollowMousePosition = !mFollowMousePosition;
+//         }
+//         Vector3 pos = transform.position;
+
+//         if (mFollowMousePosition)
+//         {
+//             pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//             // Debug.Log("Position is " + pos);
+//             pos.z = 0f;  // <-- this is VERY IMPORTANT!
+//             // Debug.Log("Screen Point:" + Input.mousePosition + "  World Point:" + p);
+//         }
+//         else
+//         {
+//             if (Input.GetKey(KeyCode.W))
+//             {
+//                 pos += ((speed * Time.smoothDeltaTime) * transform.up);
+//             }
+
+//             if (Input.GetKey(KeyCode.S))
+//             {
+//                 pos -= ((speed * Time.smoothDeltaTime) * transform.up);
+//             }
+
+//             if (Input.GetKey(KeyCode.D))
+//             {
+//                 transform.Rotate(transform.forward, -mHeroRotateSpeed * Time.smoothDeltaTime);
+//             }
+
+//             if (Input.GetKey(KeyCode.A))
+//             {
+//                 transform.Rotate(transform.forward, mHeroRotateSpeed * Time.smoothDeltaTime);
+//             }
+//         }
+//         if (Input.GetKey(KeyCode.Space))
+//         {
+//             // Prefab MUST BE locaed in Resources/Prefab folder!
+//             GameObject e = Instantiate(Resources.Load("Prefabs/Egg") as GameObject); 
+//             e.transform.localPosition = transform.localPosition;
+//             e.transform.rotation = transform.rotation;
+//             Debug.Log("Spawn Eggs:" + e.transform.localPosition);
+//         }
+//         transform.position = pos;
+//     }
+
+//     private void OnTriggerEnter2D(Collider2D collision)
+//     {
+//         Debug.Log("Here x Plane: OnTriggerEnter2D");
+//         mPlanesTouched = mPlanesTouched + 1;
+//         mEnemyCountText.text = "Planes touched = " + mPlanesTouched;
+//         Destroy(collision.gameObject);
+//         mGameGameController.EnemyDestroyed();
+//     }
+
+//     private void OnTriggerStay2D(Collider2D collision) 
+//     {
+//         Debug.Log("Here x Plane: OnTriggerStay2D");
+//     }
+// }
